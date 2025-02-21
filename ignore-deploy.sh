7884c60bfb3064685118404a649fc0fddc70eabd
mrpitch <mrpitch@outlook.com>
2025-02-21 16:59:15 +0100

    feat: use main for vercel

--- ignore-deploy.sh
+++ ignore-deploy.sh
@@ -2,7 +2,7 @@
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "vercel/main"  ]] ; then
+if [[ "$VERCEL_GIT_COMMIT_REF" == "main"  ]] ; then
   # Proceed with the build
   echo "✅ - main branch, build can proceed"
   exit 1;
